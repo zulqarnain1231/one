80d3bca8622a137d81526812e8a99f2bbf26f6ff
zulqarnain1231 <zulqarnainniazi447@gmail.com>
2023-09-17 14:06:38 -0700

    created shared wrapper and image component

--- components/Shared/Navigation/Navbar.tsx
+++ components/Shared/Navigation/Navbar.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import ComponentWrapper from "../Wrappers/ComponentWrapper";
 
 const Navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <ComponentWrapper id="navbar" style="h-[100px] w-full bg-white-main">
+      <nav className="w-full flex items-center justify-between">
+        <div className=""></div>
+      </nav>
+    </ComponentWrapper>
+  );
 };
 
 export default Navbar;
